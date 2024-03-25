944f2284b8f9f0668aa6a9dd4ebb460e5b73b6ff
Dilan Ozkaynak <dilan@proto.cx>
2024-03-25 13:17:29 -0400

    Update utm-save.js
    Signed-off-by: Dilan Ozkaynak <dilan@proto.cx>

--- utm-save.js
+++ utm-save.js
@@ -1,14 +1,28 @@
-(function() {
-  try {
-    const utmParamNames = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content'];
+(function() {  
+  try {  
+    const utmParamNames = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content'];  
     const params = new URL(location.href).searchParams;
-    for (const param of utmParamNames) {
-      const value = searchParams.get(param);
-      if (value) {
-        localStorage.setItem(param, value);
+    
+    // Function to set cookies accessible across subdomains
+    function setCookie(name, value, days) {
+      let expires = "";
+      if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
       }
+      // Set the domain to '.proto.cx' to make cookies available to proto.cx and its subdomains
+      document.cookie = name + "=" + (value || "")  + expires + "; domain=.proto.cx; path=/";
     }
-  } catch(error) {
-    console.error(error)
-  }
-}())
+    
+    for (const param of utmParamNames) {  
+      const value = params.get(param);  
+      if (value) {
+        // Call setCookie function to set cookies
+        setCookie(param, value, 7);  // Here, '7' is the number of days you want the cookies to be stored
+      }  
+    }  
+  } catch(error) {  
+    console.error(error)  
+  }  
+}());
