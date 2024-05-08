417bd2028a5c525ec684fa257db2614173454a46
Dilan Ozkaynak <dilan@proto.cx>
2024-05-08 13:46:16 -0400

    Update demo-form.js
    Signed-off-by: Dilan Ozkaynak <dilan@proto.cx>

--- demo-form.js
+++ demo-form.js
@@ -1,1 +1,10 @@
+const demoFormElement = document.getElementById('demo-form')
 
+if (demoFormElement) {
+  demoFormElement.onsubmit = (e) => {
+    e.preventDefault()
+    console.log('Form submitted');
+  }
+} else {
+  console.log('Could not find demo form');
+}
