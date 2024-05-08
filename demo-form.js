c3191973d20de24774357a214a53acdc7a6ac2bf
Dilan Ozkaynak <dilan@proto.cx>
2024-05-08 14:06:16 -0400

    Update demo-form.js
    Signed-off-by: Dilan Ozkaynak <dilan@proto.cx>

--- demo-form.js
+++ demo-form.js
@@ -4,45 +4,73 @@ const email = document.getElementById("email");
 const company = document.getElementById("company");
 const interactions = document.getElementById("messages-number-input");
 
-const cbgIndustrySelect = document.getElementById("industry-select");
-const cbgCountrySelect = document.getElementById("country-select");
+const industrySelect = document.getElementById("industry-select");
+const countrySelect = document.getElementById("country-select");
+const appSelect = document.getElementById("apps");
+const languageSelect = document.getElementById("languages");
 
-if (cbgCountrySelect) {
+if (countrySelect) {
   fetch('https://website-geolocation.proto-67a.workers.dev')
     .then(res => res.json())
     .then(res => {
-    res.countries.forEach(country => {
-      const newOption = document.createElement("option");
-      newOption.value = country.title;
-      newOption.innerText = country.title;
-      cbgCountrySelect.appendChild(newOption);
-    });
-
-    if (res.ipcountry) {
-      cbgCountrySelect.title = res.ipcountry;
-    }
-  })
+      res.countries.forEach(country => {
+        const newOption = document.createElement("option");
+        newOption.value = country.title;
+        newOption.innerText = country.title;
+        countrySelect.appendChild(newOption);
+      });
+  
+      if (res.ipcountry) {
+        countrySelect.title = res.ipcountry;
+      }
+    })
     .catch(console.error)
+} else {
+  console.error('Could not find countrySelect');
 }
 
-var formContainer = document.getElementById('demo-form-container');
-var formElement = document.getElementById('demo-form');
-formElement.addEventListener('submit', onFormSubmit);
+const formContainer = document.getElementById('demo-form-container');
+const formElement = document.getElementById('demo-form');
+
+if (formElement) {
+  formElement.addEventListener('submit', onFormSubmit);
+} else {
+  console.error('Form element not found');
+}
+
 
 function onFormSubmit(event) {
   event.preventDefault();
 
-  const selected_apps = $("#apps :selected").text();
-  const selected_languages = $("#languages :selected").text();
-  const interactions_num = interactions.value.replace(/,/g, '');
-
-  const industrySelectValue = cbgIndustrySelect.value === "Industry" ? "Other" : cbgIndustrySelect.value;
-  let countrySelectQueryStr = "";
-
-  formContainer.style.display = "none";
-  const iframeEmbedElement = document.getElementById("cbg-iframe-container");
-  iframeEmbedElement.style.display = "flex";
-    const iframeSrc = 'https://app.hubspot.com/meetings/curtis-proto/proto-demo-new?firstname=' + encodeURIComponent(firstName.value) + '&lastname=' + encodeURIComponent(lastName.value) + '&email=' + encodeURIComponent(email.value) + '&company=' + encodeURIComponent(company.value) + '&vertical=' + industrySelectValue + '&country_name=' + encodeURIComponent(cbgCountrySelect.value) + '&preferred_languages=' + encodeURIComponent(selected_languages) + '&channels_or_integrations_required=' + encodeURIComponent(selected_apps) + '&average_number_of_messages_month=' + encodeURIComponent(interactions_num);
-  iframeEmbedElement.innerHTML = '<iframe src="' + iframeSrc + '" class="cbg_iframe" frameborder="0" />'
+  if (
+    !!appSelect
+    && !!interactions
+    && !!industrySelect
+    && !!countrySelect
+    && !!languageSelect
+  ) {
+    const selectedApps = appSelect.value;
+    const selectedLanguages = languageSelect.value;
+    const industrySelectValue = industrySelect.value === "Industry" ? "Other" : industrySelect.value;
+  
+    console.log({
+      selectedApps,
+      selectedLanguages,
+      industrySelectValue,
+      firstName,
+      lastName,
+      email,
+      company,
+      interactions,
+      countrySelect
+    })
+    // formContainer.style.display = "none";
+    
+    // const iframeEmbedElement = document.getElementById("cbg-iframe-container");
+    // iframeEmbedElement.style.display = "flex";
+    
+    // const iframeSrc = 'https://app.hubspot.com/meetings/curtis-proto/proto-demo-new?firstname=' + encodeURIComponent(firstName.value) + '&lastname=' + encodeURIComponent(lastName.value) + '&email=' + encodeURIComponent(email.value) + '&company=' + encodeURIComponent(company.value) + '&vertical=' + industrySelectValue + '&country_name=' + encodeURIComponent(countrySelect.value) + '&preferred_languages=' + encodeURIComponent(selected_languages) + '&channels_or_integrations_required=' + encodeURIComponent(selected_apps) + '&average_number_of_messages_month=' + encodeURIComponent(interactions_num);
+    // iframeEmbedElement.innerHTML = '<iframe src="' + iframeSrc + '" class="cbg_iframe" frameborder="0" />'
+  }
   return false;
 }
